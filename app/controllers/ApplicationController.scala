005d5b4be33e366f50fd3aef601b493d458cc2bf
alex <figwam@gmx.de>
2016-02-01 01:42:12 +0100

    createStudioClazzDef

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -8,7 +8,7 @@ import com.mohiva.play.silhouette.api.{ Environment, LogoutEvent, Silhouette }
 import com.mohiva.play.silhouette.impl.authenticators.JWTAuthenticator
 import com.mohiva.play.silhouette.impl.providers.SocialProviderRegistry
 import models._
-import models.daos.{RegistrationDAO, OfferDAO, ClazzDAO}
+import models.daos.{RegistrationDAO}
 import play.Play
 import play.api.Logger
 import play.api.cache.Cache
@@ -35,71 +35,9 @@ class ApplicationController @Inject()(
                                        val messagesApi: MessagesApi,
                                        val env: Environment[User, JWTAuthenticator],
                                        socialProviderRegistry: SocialProviderRegistry,
-                                       clazzDAO: ClazzDAO,
-                                       offerDAO: OfferDAO)
+                                       cService: ClazzService,
+                                       oService: OfferService)
   extends Silhouette[User, JWTAuthenticator] {
-
-
-
-
-  def enums = UserAwareAction.async { implicit request =>
-    lazy val cacheExpire = Play.application().configuration().getString("cache.expire.get.enums").toInt
-    val enums:JsValue = Cache.getAs[JsValue]("enums").getOrElse{
-      var recurrences = ArrayBuffer[JsValue]()
-      for (d <- Recurrence.values) {
-        recurrences += Json.obj(
-          "id" -> d,
-          "name" -> Messages("enum.clazz.recurrence."+d)
-        )
-      }
-      var types = ArrayBuffer[JsValue]()
-      for (d <- Type.values) {
-        types += Json.obj(
-          "id" -> d,
-          "name" -> Messages("enum.clazz.type."+d)
-        )
-      }
-      val e = Json.obj("enums" -> Json.obj(
-        "clazz" -> Json.obj(
-          "recurrences" -> Json.toJson(recurrences),
-          "types" -> Json.toJson(types)
-        )
-      ))
-      Cache.set("enums", e, cacheExpire.seconds)
-      e
-    }
-    Future.successful(Ok(enums))
-  }
-
-
-  def offers = UserAwareAction.async { implicit request =>
-    lazy val cacheExpire = Play.application().configuration().getString("cache.expire.get.offers").toInt
-    val offers:List[Offer] = Cache.getAs[List[Offer]]("offers").getOrElse{
-      val offers:List[Offer] = Await.result(offerDAO.list(), 5.seconds)
-      Cache.set("offers", offers, cacheExpire.seconds)
-      offers
-    }
-    Future.successful(Ok(Json.toJson(offers)))
-  }
-  /**
-   * Returns the partner.
-   *
-   * @return The result to display.
-   */
-  def partner = SecuredAction.async { implicit request =>
-    request.identity match {
-      case p:Partner => Future.successful(Ok(Json.toJson(p)))
-      case _ => Future.successful(InternalServerError)
-    }
-  }
-
-  /**
-   * Manages the sign out action.
-   */
-  def signOut = SecuredAction.async { implicit request =>
-    env.eventBus.publish(LogoutEvent(request.identity, request, request2Messages))
-    env.authenticatorService.discard(request.authenticator, Ok)
-  }
 
   /**
    * Provides the desired template.
--- app/modules/AppModule.scala
+++ app/modules/AppModule.scala
@@ -2,13 +2,14 @@ package modules
 
 import com.google.inject.AbstractModule
 import models._
-import models.daos._
 import net.codingwell.scalaguice.ScalaModule
+import play.api.libs.concurrent.AkkaGuiceSupport
+import workers.{ClazzScheduler, DBLogAdmin}
 
 /**
  * Created by alex on 28/09/15.
  */
-class AppModule extends AbstractModule with ScalaModule {
+class AppModule extends AbstractModule with ScalaModule with AkkaGuiceSupport {
 
   /**
    * Configures the module.
@@ -16,5 +17,9 @@ class AppModule extends AbstractModule with ScalaModule {
   def configure() {
     bind[UserService].to[PartnerService]
     bind[AddressService].to[AddressServicePartnerImpl]
+
+
+    bindActor[ClazzScheduler]("ClazzScheduler")
+    bindActor[DBLogAdmin]("DBLogAdmin")
   }
 }
